e24d7a638eaa90b5e7cb2871d7bb6daa56c81497
Anushka Sagar <anushkas@usc.edu>
2017-11-10 21:28:07 -0800

    added app.js

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
+
+// import {
+//   StackNavigator,
+// } from 'react-navigation';
+
+//  const App = StackNavigator({
+//   LoginScreen: { screen: LoginScreen },
+//   ProfileScreen: { screen: ProfileScreen },
+// });
 import React, {Component} from 'react';
+import { StackNavigator } from 'react-navigation';
 import {StyleSheet, View, Image, Text, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
-export default class Login extends Component{
+
+
+class LoginScreen extends Component{
+  static navigationOptions = {
+    title: 'MazuMah'
+  };
   render(){
+    const { navigate } = this.props.navigation;
     return(
       <KeyboardAvoidingView behavior = "padding" style={styles.container}>
         <View style={styles.logoContainer}>
-          <Text style = {styles.title}>201 CryptoDashboard</Text>
+          <Text style = {styles.title}>MazuMah</Text>
         </View>
         <View style={styles.formContainer}>
+         <Image
+         
+          style={styles.image}
+          source={require('./MMLogo.png')}
+        />
+          
           <TextInput
             placeholder ="Username"
             placeholderTextColor = "#808080"
@@ -15,23 +37,32 @@ export default class Login extends Component{
             style = {styles.input}
             onSubmitEditing={() => this.pwInput.focus()}
             />
+           
+            
           <TextInput
             placeholder ="Password"
             placeholderTextColor = "#808080"
+            secureTextEntry={true}
             returnKeyType = "go"
             style = {styles.input1}
             ref= {(input) => this.pwInput = input}
             />
+            <View>
+
+              </View>
           <View style={styles.buttonContainer}>
             <TouchableOpacity>
-              <Text style = {styles.buttonText}>Create Account</Text>
+           
+               <Text style = {styles.buttonText}>Create Account</Text>
             </TouchableOpacity>
+           </View>
+          <View style={styles.buttonContainer1}>
             <TouchableOpacity>
-              <Text style = {styles.buttonText}>Login</Text>
+              <Text style = {styles.buttonText1}>Login</Text>
             </TouchableOpacity>
           </View>
           <TouchableOpacity style = {styles.contNoAccount}>
-            <Text style = {styles.buttonText}>Continue without account</Text>
+            <Text style = {styles.buttonText}>Continue Without Account</Text>
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
@@ -42,7 +73,11 @@ export default class Login extends Component{
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    backgroundColor: '#3498db'
+    backgroundColor: '#000000'
+  },
+  image:{
+     width: 360, 
+     height: 300
   },
   logoContainer:{
     alignItems: 'center',
@@ -51,10 +86,12 @@ const styles = StyleSheet.create({
     paddingLeft: 300,
   },
   title:{
-    color: '#9400d3',
-    width: 600,
+    color: '#D3A15D',
+    //textAlign: 'center',
+    fontFamily: 'Courier New',
+    width: 478,
     fontWeight: 'bold',
-    fontSize: 20
+    fontSize: 40
   },
   input:{
     borderRadius: 10,
@@ -77,17 +114,66 @@ const styles = StyleSheet.create({
     width: 150
   },
   buttonContainer:{
+    alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingLeft: 50,
-    paddingRight: 50,
+    paddingRight: 60,
     marginBottom: 50,
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: '#D3A15D'
+  },
+  buttonContainer1:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 50,
+    paddingRight: 60,
+    marginBottom: 50,
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: '#D3A15D'
   },
   contNoAccount:{
     alignItems: 'center',
-    marginBottom: 100
+    marginBottom: 100,
+    borderWidth: 1,
+    borderColor: '#D3A15D'
   },
   buttonText:{
-    color: 'purple'
+    color: '#D3A15D',
+    fontFamily: 'Courier New',
+    fontWeight: 'bold',
+  },
+  buttonText1:{
+    color: '#D3A15D',
+    fontFamily: 'Courier New',
+    fontWeight: 'bold',
   }
 })
+
+class ProfileScreen extends Component{
+  render(){
+    <Text>Profile Page</Text>
+  }
+
+
+}
+
+const SimpleApp = StackNavigator({
+  Login: { screen: LoginScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+export default class App extends React.Component {
+  static navigationOptions = {
+    title: 'Profile Page',
+  };
+  render() {
+    return <SimpleApp />;
+  }
+}
+
+
+
